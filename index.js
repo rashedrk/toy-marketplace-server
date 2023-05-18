c9c081c8bcd1c3b152371de6ebd9a1179fc235d8
rashedrk <rashedrk001@gmail.com>
2023-05-18 15:29:42 +0600

    delete a single toy

--- index.js
+++ index.js
@@ -74,6 +74,14 @@ async function run() {
             res.send(result);
         })
 
+        
+        // delete a single toy 
+        app.delete('/toys/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.deleteOne(query);
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
